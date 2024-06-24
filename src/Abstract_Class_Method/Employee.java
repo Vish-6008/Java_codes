79f869821fcb4d518e8b6774fa92580a0b415d72
Vish-6008 <vishwanath.d@crevavi.com>
2024-06-24 17:38:58 +0530

    Update Employee.java

--- src/Abstract_Class_Method/Employee.java
+++ src/Abstract_Class_Method/Employee.java
@@ -1,12 +1,13 @@
 /********************************************************************************************
- *   COPYRIGHT (C) 2022 TONAL INC
+ *  COPYRIGHT (C) 2024 CREVAVI TECHNOLOGIES PVT LTD
  *   The reproduction, transmission or use of this document/file or its
- *   contents is not permitted without  written authorization.
+ *   contents is not permitted without written authorization.
  *   Offenders will be liable for damages. All rights reserved.
  *---------------------------------------------------------------------------
  *   Purpose: Below code show's the flow of Abstract Keywords and Methods.
  *   Project: Abstract[Class] Person
- *   Platform: JavaSE-22.0.1
+ *   Platform: Cross-platform (Windows, macOS, Linux)
+ *   Compiler: JDK-22
  *   IDE:  	   Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
  *	 Version: 2024-03 (4.31.0)
  *   Build id: 20240307-1437
