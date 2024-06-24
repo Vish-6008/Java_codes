a4cedaf5f9d0bbd565457006373657e30538ad8b
Vish-6008 <vishwanath.d@crevavi.com>
2024-06-24 17:55:46 +0530

    Update Demo2.java

--- src/Abstract_Examples/Demo2.java
+++ src/Abstract_Examples/Demo2.java
@@ -1,19 +1,18 @@
 /********************************************************************************************
- *   COPYRIGHT (C) 2022 TONAL INC
+ *   COPYRIGHT (C) 2024 CREVAVI TECHNOLOGIES PVT LTD
  *   The reproduction, transmission or use of this document/file or its
- *   contents is not permitted without  written authorization.
+ *   contents is not permitted without written authorization.
  *   Offenders will be liable for damages. All rights reserved.
  *---------------------------------------------------------------------------
- *   Purpose:  JiraRestApiUtils.java
- *   Project:  Tonal Logistics
- *   Platform: Cloud Service Provider Independent
+ *   Purpose:  Abstract methods are created.
+ *   Project:  Demo2
+ *   Platform: Cross-platform (Windows, macOS, Linux)
+ *   Compiler: JDK-22
  *   Compiler: JavaSE-11
  *   IDE:	   Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
-
- *			   Version: 2022-02 (4.0.0)
- *             Build id: 202202
+ *				Version: 2022-02 (4.0.0)
+ *  			Build id: 202202
  ********************************************************************************************/
-
 
 
 package Abstract_Examples;								//Abstract methods 
